f04649c84ffd47660e61c6b096fddae3fb240348
JurajDoncevic <24619666+JurajDoncevic@users.noreply.github.com>
2023-12-25 23:42:03 +0100

    updated disconnected lens to definition

---
+++ Bifrons.Lenses.Tests/Symmetric/Strings/DisconnectLensTests.cs
@@ -0,0 +1,16 @@
+﻿using Bifrons.Lenses.Tests;
+
+namespace Bifrons.Lenses.Symmetric.Strings.Tests;
+
+public class DisconnectLensTests : SymmetricLensTestingFramework<string, string>
+{
+    protected override string _left => "15000kn";
+
+    protected override string _right => "unk";
+
+    protected override string _updatedLeft => "10000kn";
+
+    protected override string _updatedRight => "unknown";
+
+    protected override BaseSymmetricLens<string, string> _lens => DisconnectLens.Cons(@"\+dkn", "", "unk", "");
+}
--- Bifrons.Lenses/Symmetric/Strings/DisconnectLens.cs
+++ Bifrons.Lenses/Symmetric/Strings/DisconnectLens.cs
@@ -1,6 +1,42 @@
-﻿namespace Bifrons.Lenses;
+﻿using System.Text.RegularExpressions;
 
-public class DisconnectLens
+namespace Bifrons.Lenses;
+
+public sealed class DisconnectLens : BaseSymmetricLens<string, string>
 {
+    private readonly Regex _matchToRightRegex;
+    private readonly Regex _matchToLeftRegex;
+    private readonly string _rightDefault;
+    private readonly string _leftDefault;
 
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="matchToRightRegex">Regex string that is used for PutR</param>
+    /// <param name="matchToLeftRegex">Regex string that is used for PutL</param>
+    private DisconnectLens(string matchToRightRegex, string matchToLeftRegex, string leftDefault, string rightDefault)
+    {
+        _matchToRightRegex = new Regex(matchToRightRegex ?? string.Empty);
+        _matchToLeftRegex = new Regex(matchToLeftRegex ?? string.Empty);
+        _rightDefault = rightDefault ?? string.Empty;
+        _leftDefault = leftDefault ?? string.Empty;
+    }
+    public override Func<string, Option<string>, Result<string>> PutLeft =>
+        (_, originalLeft) => originalLeft
+            ? Results.OnSuccess(originalLeft.Value)
+            : Results.OnSuccess(string.Empty);
+
+    public override Func<string, Option<string>, Result<string>> PutRight =>
+        (_, originalRight) => originalRight
+            ? Results.OnSuccess(originalRight.Value)
+            : Results.OnSuccess(string.Empty);
+
+    public override Func<string, Result<string>> CreateRight =>
+        left => Results.OnSuccess(_rightDefault);
+
+    public override Func<string, Result<string>> CreateLeft =>
+        right => Results.OnSuccess(_leftDefault);
+
+    public static DisconnectLens Cons(string matchToRightRegex, string matchToLeftRegex, string leftDefault, string rightDefault)
+        => new(matchToRightRegex, matchToLeftRegex, leftDefault, rightDefault);
 }
