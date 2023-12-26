d83b5a883db5032a2674863d08c1e36de7646fd2
JurajDoncevic <24619666+JurajDoncevic@users.noreply.github.com>
2023-12-26 20:43:39 +0100

    added experiments tests

---
+++ Bifrons.Lenses.Tests/Symmetric/Strings/_Experiments.cs
@@ -0,0 +1,42 @@
+﻿using Bifrons.Lenses.Symmetric.Strings;
+
+namespace Bifrons.Lenses.Tests;
+
+public class Experiments
+{
+    [Fact]
+    public void CreateRight_WithConcatLens()
+    {
+        var lens = IdentityLens.Cons(@"\w+") |
+            IdentityLens.Cons(@"\s+") |
+            IdentityLens.Cons(@"(?!\w+\s+)\w+") |
+            DeleteLens.Cons(@"(?!\w+\s+):\s+") |
+            InsertLens.Cons(@",");
+
+        var source = "Jane Doe: ";
+        var expectedTarget = "Jane Doe,";
+
+        var target = lens.CreateRight(source);
+
+        Assert.Equal(expectedTarget, target);
+
+    }
+
+    [Fact]
+    public void PutRight_OnConcatLens_WithNoOriginalSource()
+    {
+        var lens = IdentityLens.Cons(@"\w+") |
+            IdentityLens.Cons(@"\s+") |
+            IdentityLens.Cons(@"(?!\w+\s+)\w+") |
+            DeleteLens.Cons(@"(?!\w+\s+):\s+") |
+            InsertLens.Cons(@",");
+
+        var source = "Jane Doe: ";
+        var expectedTarget = "Jane Doe,";
+
+        var target = lens.PutRight(source, Option.None<string>());
+
+        Assert.Equal(expectedTarget, target);
+
+    }
+}
