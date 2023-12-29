c28289b75ed15ff2dfa65a3488c74ebc66c45909
JurajDoncevic <24619666+JurajDoncevic@users.noreply.github.com>
2023-12-29 10:13:26 +0100

    Added Either monad

---
+++ Bifrons.Base.Tests/EitherTests.cs
@@ -0,0 +1,151 @@
+﻿namespace Bifrons.Base.Tests;
+
+public class EitherTests
+{
+    [Fact]
+    public void CreateEither_WithLeft()
+    {
+        var either = Either.Left<string, int>("left");
+        Assert.True(either.IsLeft);
+        Assert.False(either.IsRight);
+        Assert.Equal("left", either.Left);
+    }
+
+    [Fact]
+    public void CreateEither_WithRight()
+    {
+        var either = Either.Right<string, int>(42);
+        Assert.False(either.IsLeft);
+        Assert.True(either.IsRight);
+        Assert.Equal(42, either.Right);
+    }
+
+    [Fact]
+    public void LeftEither_MapLeft()
+    {
+        var either = Either.Left<string, int>("left");
+        var mapped = either.MapLeft(s => s.Length);
+        Assert.True(mapped.IsLeft);
+        Assert.False(mapped.IsRight);
+        Assert.Equal(4, mapped.Left);
+    }
+
+    [Fact]
+    public void RightEither_MapLeft()
+    {
+        var either = Either.Right<string, int>(42);
+        var mapped = either.MapLeft(s => s.Length);
+        Assert.False(mapped.IsLeft);
+        Assert.True(mapped.IsRight);
+        Assert.Equal(42, mapped.Right);
+    }
+
+    [Fact]
+    public void LeftEither_MapRight()
+    {
+        var either = Either.Left<string, int>("left");
+        var mapped = either.MapRight(i => i * 2);
+        Assert.True(mapped.IsLeft);
+        Assert.False(mapped.IsRight);
+        Assert.Equal("left", mapped.Left);
+    }
+
+    [Fact]
+    public void RightEither_MapRight()
+    {
+        var either = Either.Right<string, int>(42);
+        var mapped = either.MapRight(i => i * 2);
+        Assert.False(mapped.IsLeft);
+        Assert.True(mapped.IsRight);
+        Assert.Equal(84, mapped.Right);
+    }
+
+    [Fact]
+    public void LeftEither_Map()
+    {
+        var either = Either.Left<string, int>("left");
+        var mapped = either.Map(
+            s => s.Length,
+            i => i * 2);
+        Assert.True(mapped.IsLeft);
+        Assert.False(mapped.IsRight);
+        Assert.Equal(4, mapped.Left);
+    }
+
+    [Fact]
+    public void RightEither_Map()
+    {
+        var either = Either.Right<string, int>(42);
+        var mapped = either.Map(
+            s => s.Length,
+            i => i * 2);
+        Assert.False(mapped.IsLeft);
+        Assert.True(mapped.IsRight);
+        Assert.Equal(84, mapped.Right);
+    }
+
+    [Fact]
+    public void LeftEither_BindLeft()
+    {
+        var either = Either.Left<string, int>("left");
+        var mapped = either.BindLeft(s => Either.Left<int, int>(s.Length));
+        Assert.True(mapped.IsLeft);
+        Assert.False(mapped.IsRight);
+        Assert.Equal(4, mapped.Left);
+    }
+
+    [Fact]
+    public void RightEither_BindLeft()
+    {
+        var either = Either.Right<string, int>(42);
+        var mapped = either.BindLeft(s => Either.Left<int, int>(s.Length));
+        Assert.False(mapped.IsLeft);
+        Assert.True(mapped.IsRight);
+        Assert.Equal(42, mapped.Right);
+    }
+
+    [Fact]
+    public void LeftEither_BindRight()
+    {
+        var either = Either.Left<string, int>("left");
+        var mapped = either.BindRight(i => Either.Right<string, int>(i * 2));
+        Assert.True(mapped.IsLeft);
+        Assert.False(mapped.IsRight);
+        Assert.Equal("left", mapped.Left);
+    }
+
+    [Fact]
+    public void RightEither_BindRight()
+    {
+        var either = Either.Right<string, int>(42);
+        var mapped = either.BindRight(i => Either.Right<string, int>(i * 2));
+        Assert.False(mapped.IsLeft);
+        Assert.True(mapped.IsRight);
+        Assert.Equal(84, mapped.Right);
+    }
+
+    [Fact]
+    public void LeftEither_Bind()
+    {
+        var either = Either.Left<string, int>("left");
+        var mapped = either.Bind(
+            s => Either.Left<int, int>(s.Length),
+            i => Either.Right<int, int>(i * 2));
+        Assert.True(mapped.IsLeft);
+        Assert.False(mapped.IsRight);
+        Assert.Equal(4, mapped.Left);
+    }
+
+    [Fact]
+    public void RightEither_Bind()
+    {
+        var either = Either.Right<string, int>(42);
+        var mapped = either.Bind(
+            s => Either.Left<int, int>(s.Length),
+            i => Either.Right<int, int>(i * 2));
+        Assert.False(mapped.IsLeft);
+        Assert.True(mapped.IsRight);
+        Assert.Equal(84, mapped.Right);
+    }
+
+}
---
+++ Bifrons.Base/Either.cs
@@ -0,0 +1,104 @@
+﻿namespace Bifrons.Base;
+/// <summary>
+/// Either monad
+/// </summary>
+/// <typeparam name="TLeft"></typeparam>
+/// <typeparam name="TRight"></typeparam>
+public readonly struct Either<TLeft, TRight>
+{
+    private readonly Option<TLeft> _left;
+    private readonly Option<TRight> _right;
+
+    /// <summary>
+    /// Is this a left value?
+    /// </summary>
+    public bool IsLeft => _left.IsSome;
+
+    /// <summary>
+    /// Is this a right value?
+    /// </summary>
+    public bool IsRight => _right.IsSome;
+
+    /// <summary>
+    /// Left value
+    /// </summary>
+    public TLeft Left => _left.Value;
+
+    /// <summary>
+    /// Right value
+    /// </summary>
+    public TRight Right => _right.Value;
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    internal Either(Option<TLeft> left, Option<TRight> right)
+    {
+        _left = left;
+        _right = right;
+    }
+}
+
+/// <summary>
+/// Either monad
+/// </summary>
+public static class Either
+{
+    /// <summary>
+    /// Create a left value
+    /// </summary>
+    public static Either<TLeft, TRight> Left<TLeft, TRight>(TLeft value)
+        => new Either<TLeft, TRight>(value, Option.None<TRight>());
+
+    /// <summary>
+    /// Create a right value
+    /// </summary>
+    public static Either<TLeft, TRight> Right<TLeft, TRight>(TRight value)
+        => new Either<TLeft, TRight>(Option.None<TLeft>(), value);
+
+
+    public static Either<RLeft, RRight> Map<TLeft, TRight, RLeft, RRight>(
+        this Either<TLeft, TRight> either,
+        Func<TLeft, RLeft> lmap,
+        Func<TRight, RRight> rmap)
+        => either.IsLeft
+            ? Left<RLeft, RRight>(lmap(either.Left))
+            : Right<RLeft, RRight>(rmap(either.Right));
+
+    public static Either<RLeft, TRight> MapLeft<TLeft, TRight, RLeft>(
+        this Either<TLeft, TRight> either,
+        Func<TLeft, RLeft> lmap)
+        => either.IsLeft
+            ? Left<RLeft, TRight>(lmap(either.Left))
+            : Right<RLeft, TRight>(either.Right);
+
+    public static Either<TLeft, RRight> MapRight<TLeft, TRight, RRight>(
+        this Either<TLeft, TRight> either,
+        Func<TRight, RRight> rmap)
+        => either.IsLeft
+            ? Left<TLeft, RRight>(either.Left)
+            : Right<TLeft, RRight>(rmap(either.Right));
+
+    public static Either<RLeft, RRight> Bind<TLeft, TRight, RLeft, RRight>(
+        this Either<TLeft, TRight> either,
+        Func<TLeft, Either<RLeft, RRight>> lbind,
+        Func<TRight, Either<RLeft, RRight>> rbind)
+        => either.IsLeft
+            ? lbind(either.Left)
+            : rbind(either.Right);
+
+    public static Either<RLeft, TRight> BindLeft<TLeft, TRight, RLeft>(
+        this Either<TLeft, TRight> either,
+        Func<TLeft, Either<RLeft, TRight>> lbind)
+        => either.IsLeft
+            ? lbind(either.Left)
+            : Right<RLeft, TRight>(either.Right);
+
+    public static Either<TLeft, RRight> BindRight<TLeft, TRight, RRight>(
+        this Either<TLeft, TRight> either,
+        Func<TRight, Either<TLeft, RRight>> rbind)
+        => either.IsLeft
+            ? Left<TLeft, RRight>(either.Left)
+            : rbind(either.Right);
+}
+
