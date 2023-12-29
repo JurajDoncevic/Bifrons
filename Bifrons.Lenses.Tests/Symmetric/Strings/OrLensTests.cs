ccf0a2cd6acc7c7ab608a5aac18fbade60cf8654
JurajDoncevic <24619666+JurajDoncevic@users.noreply.github.com>
2023-12-29 16:29:02 +0100

    or lens tests added

---
+++ Bifrons.Lenses.Tests/Symmetric/Strings/OrLensTests.cs
@@ -0,0 +1,17 @@
+﻿using Bifrons.Lenses.Tests;
+
+namespace Bifrons.Lenses.Symmetric.Strings.Tests;
+
+public class OrLensTests : SymmetricLensTestingFramework<Either<string, string>, Either<string, string>>
+{
+    protected override Either<string, string> _left => Either.Left<string, string>("John");
+
+    protected override Either<string, string> _right => Either.Right<string, string>("123");
+
+    protected override Either<string, string> _updatedLeft => Either.Left<string, string>("Jonathan");
+
+    protected override Either<string, string> _updatedRight => Either.Right<string, string>("1234");
+
+    protected override BaseSymmetricLens<Either<string, string>, Either<string, string>> _lens =>
+        IdentityLens.Cons(@"\w+") + IdentityLens.Cons(@"\d+");
+}
