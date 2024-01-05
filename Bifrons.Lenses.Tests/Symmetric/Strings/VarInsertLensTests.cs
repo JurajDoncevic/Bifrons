5349940e2c0d66cf8add6cccc03811d9777b3fc2
JurajDoncevic <24619666+JurajDoncevic@users.noreply.github.com>
2024-01-05 19:37:38 +0100

    added variable insert lens

---
+++ Bifrons.Lenses.Tests/Symmetric/Strings/VarInsertLensTests.cs
@@ -0,0 +1,21 @@
+﻿using Bifrons.Lenses.Tests;
+
+namespace Bifrons.Lenses.Symmetric.Strings.Tests;
+
+public class VarInsertLensTests : SymmetricLensTestingFramework<string, string>
+{
+    protected override string _left => "";
+
+    protected override string _right => "1234";
+
+    protected override BaseSymmetricLens<string, string> _lens =>
+        VarInsertLens.Cons(@"\d+", "0000");
+
+    [Fact]
+    public void CreateRightDefault()
+    {
+        var result = _lens.CreateRight("");
+        Assert.True(result);
+        Assert.Equal("0000", result.Data);
+    }
+}
