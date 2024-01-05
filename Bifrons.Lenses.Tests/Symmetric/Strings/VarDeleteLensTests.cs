7f70006f89571b5beb24b4497d31d03454249679
JurajDoncevic <24619666+JurajDoncevic@users.noreply.github.com>
2024-01-05 19:28:54 +0100

    added a variable del lens

---
+++ Bifrons.Lenses.Tests/Symmetric/Strings/VarDeleteLensTests.cs
@@ -0,0 +1,21 @@
+﻿using Bifrons.Lenses.Tests;
+
+namespace Bifrons.Lenses.Symmetric.Strings.Tests;
+
+public class VarDeleteLensTests : SymmetricLensTestingFramework<string, string>
+{
+    protected override string _left => "1234";
+
+    protected override string _right => "";
+
+    protected override BaseSymmetricLens<string, string> _lens =>
+        VarDeleteLens.Cons(@"\d+", "0000");
+
+    [Fact]
+    public void CreateLeftDefault()
+    {
+        var result = _lens.CreateLeft("");
+        Assert.True(result);
+        Assert.Equal("0000", result.Data);
+    }
+}
---
+++ Bifrons.Lenses/Symmetric/Strings/VarDeleteLens.cs
@@ -0,0 +1,25 @@
+﻿namespace Bifrons.Lenses.Symmetric.Strings;
+
+/// <summary>
+/// Describes a variable delete lens that uses a regex to delete part of a string. In case of no source, the default value is provided.
+/// vardel(regex, default) : string <=> string
+/// </summary>
+public class VarDeleteLens : DisconnectLens
+{
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="deleteRegex">Regex to delete</param>
+    /// <param name="defaultConstant">Default value to provide in case of no source. Should match regex!</param>
+    protected VarDeleteLens(string deleteRegex, string defaultConstant)
+        : base(deleteRegex, string.Empty, defaultConstant, string.Empty)
+    { }
+
+    /// <summary>
+    /// Constructs a variable delete lens
+    /// </summary>
+    /// <param name="deleteRegex">Regex to delete</param>
+    /// <param name="defaultConstant">Default value to provide in case of no source. Should match regex!</param>
+    public static VarDeleteLens Cons(string deleteRegex, string defaultConstant)
+        => new(deleteRegex, defaultConstant);
+}
