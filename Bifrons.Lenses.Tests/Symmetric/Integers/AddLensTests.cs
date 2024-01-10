0db10f733f214e910449ddae2de36ee8dd3126c5
Juraj Doncevic <juraj.doncevic@fer.hr>
2024-01-10 14:05:40 +0100

    added integer lens tests

---
+++ Bifrons.Lenses.Tests/Symmetric/Integers/AddLensTests.cs
@@ -0,0 +1,18 @@
+﻿using Bifrons.Lenses.Tests;
+
+namespace Bifrons.Lenses.Symmetric.Integers.Tests;
+
+public class AddLensTests : SymmetricLensTestingFramework<int, int>
+{
+    protected override int _left => 1;
+
+    protected override int _right => 6;
+
+    protected override (int originalSource, int expectedOriginalTarget, int updatedTarget, int expectedUpdatedSource) _roundTripWithRightSideUpdateData 
+        => (1, 6, 9, 4);
+
+    protected override (int originalSource, int expectedOriginalTarget, int updatedTarget, int expectedUpdatedSource) _roundTripWithLeftSideUpdateData 
+        => (7, 2, 5, 10);
+
+    protected override BaseSymmetricLens<int, int> _lens => AddLens.Cons(5);
+}
---
+++ Bifrons.Lenses.Tests/Symmetric/Integers/SubLensTests.cs
@@ -0,0 +1,18 @@
+﻿using Bifrons.Lenses.Tests;
+
+namespace Bifrons.Lenses.Symmetric.Integers.Tests;
+
+public class SubLensTests : SymmetricLensTestingFramework<int, int>
+{
+    protected override int _left => 6;
+
+    protected override int _right => 1;
+
+    protected override (int originalSource, int expectedOriginalTarget, int updatedTarget, int expectedUpdatedSource) _roundTripWithRightSideUpdateData 
+        => (7, 2, 5, 10);
+
+    protected override (int originalSource, int expectedOriginalTarget, int updatedTarget, int expectedUpdatedSource) _roundTripWithLeftSideUpdateData 
+        => (1, 6, 9, 4);
+
+    protected override BaseSymmetricLens<int, int> _lens => SubLens.Cons(5);
+}
