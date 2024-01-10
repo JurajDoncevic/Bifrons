77cfd89dddcf585509756324d8f19796e2b41d66
Juraj Doncevic <juraj.doncevic@fer.hr>
2024-01-10 19:42:29 +0100

    completed comment docs for integer-string lens

--- Bifrons.Lenses/Symmetric/CrossType/IntegerStringLens.cs
+++ Bifrons.Lenses/Symmetric/CrossType/IntegerStringLens.cs
@@ -2,10 +2,16 @@
 
 namespace Bifrons.Lenses.Symmetric.CrossType;
 
+/// <summary>
+/// Describes an integer-string lens that tranforms integers to strings and vice versa.
+/// IntStr() : int <=> string
 public sealed class IntegerStringLens : BaseSymmetricLens<int, string>
 {
     private readonly Regex _numberRegex = new(@"-?\d+");
 
+    /// <summary>
+    /// Constructor
+    /// </summary>
     private IntegerStringLens()
     {
     }
@@ -48,14 +54,27 @@ public sealed class IntegerStringLens : BaseSymmetricLens<int, string>
             return Results.OnSuccess(int.Parse(match.Value));
         };
     
+    /// <summary>
+    /// Constructs an integer-string lens
+    /// </summary>
     public static IntegerStringLens Cons() => new();
 }
 
+/// <summary>
+/// Describes an integer-string lens that tranforms strings to integers and vice versa.
+/// StrInt() : string <=> int
+/// </summary>
 public sealed class StringIntegerLens : InvertLens<string, int>
 {
+    /// <summary>
+    /// Constructor
+    /// </summary>
     private StringIntegerLens() : base(IntegerStringLens.Cons())
     {
     }
 
+    /// <summary>
+    /// Constructs an integer-string lens
+    /// </summary>
     public static StringIntegerLens Cons() => new();
 }
