2087391c454ee529e8727614fb140da9c52cfd56
JurajDoncevic <juraj.doncevic@fer.hr>
2024-02-12 16:19:56 +0100

    added identity lens for integers

---
+++ Bifrons.Lenses/Symmetric/Integers/IdentityLens.cs
@@ -0,0 +1,18 @@
+﻿namespace Bifrons.Lenses.Symmetric.Integers;
+
+public sealed class IdentityLens : SymmetricIntegerLens
+{
+    public override Func<int, Option<int>, Result<int>> PutLeft => 
+        (int updatedView, Option<int> _) => updatedView;
+
+    public override Func<int, Option<int>, Result<int>> PutRight => 
+        (int updatedView, Option<int> _) => updatedView;
+
+    public override Func<int, Result<int>> CreateRight => 
+        source => source;
+
+    public override Func<int, Result<int>> CreateLeft => 
+        source => source;
+
+    public static IdentityLens Cons() => new();
+}
