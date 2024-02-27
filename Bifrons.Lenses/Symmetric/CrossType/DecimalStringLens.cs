fa76f1a070f1e3186b39bbe712159cdfddb0d4bf
JurajDoncevic <juraj.doncevic@fer.hr>
2024-02-27 13:20:45 +0100

    added crosstype lenses

---
+++ Bifrons.Lenses/Symmetric/CrossType/DecimalStringLens.cs
@@ -0,0 +1,51 @@
+﻿using Bifrons.Lenses.Symmetric;
+
+namespace Bifrons.Lenses;
+
+/// <summary>
+/// Describes a decimal-string lens that tranforms decimals to strings and vice versa.
+/// DecStr : decimal <=> string
+/// </summary>
+public sealed class DecimalStringLens : BaseSymmetricLens<double, string>
+{
+    private DecimalStringLens()
+    {
+    }
+
+    public override Func<string, Option<double>, Result<double>> PutLeft => 
+        (string updatedView, Option<double> _) => 
+            double.Parse(updatedView);
+
+    public override Func<double, Option<string>, Result<string>> PutRight => 
+        (double updatedView, Option<string> _) => 
+            updatedView.ToString();
+
+    public override Func<double, Result<string>> CreateRight => 
+        source => source.ToString();
+
+    public override Func<string, Result<double>> CreateLeft => 
+        source => double.Parse(source);
+
+    /// <summary>
+    /// Constructs a double string lens.
+    /// </summary>
+    public static DecimalStringLens Cons() => new();
+}
+
+
+/// <summary>
+/// Describes a string double lens that tranforms strings to decimals and vice versa.
+/// StrDec : string <=> decimal
+/// </summary>
+public sealed class StringDecimalLens: InvertLens<string, double>
+{
+
+    private StringDecimalLens() : base(DecimalStringLens.Cons())
+    {
+    }
+
+    /// <summary>
+    /// Constructs a string double lens.
+    /// </summary>
+    public static StringDecimalLens Cons() => new();
+}
