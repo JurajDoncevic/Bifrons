69666a858b212746dd0e740903ac54ed42018882
JurajDoncevic <juraj.doncevic@fer.hr>
2024-03-26 12:50:32 +0100

    initial join lens

--- Bifrons.Lenses/Symmetric/Relational/Tables/JoinLens.cs
+++ Bifrons.Lenses/Symmetric/Relational/Tables/JoinLens.cs
@@ -1,7 +1,7 @@
 ﻿using Bifrons.Lenses.Symmetric.Relational.Model;
 
 namespace Bifrons.Lenses.Symmetric.Relational.Tables;
-public sealed class JoinLens : SymmetricTableLens
+public sealed class JoinLens : ISymmetricLens<(Table, Table), Table>
 {
     private readonly string _targetTableName;
     private readonly SymmetricTableLens _leftTableLens;
@@ -9,7 +9,7 @@ public sealed class JoinLens : SymmetricTableLens
     private readonly Column _leftKeyColumn;
     private readonly Column _rightKeyColumn;
 
-    public override string TargetTableName => _targetTableName;
+    public string TargetTableName => _targetTableName;
 
     private JoinLens(string targetTableName, SymmetricTableLens leftTableLens, SymmetricTableLens rightTableLens, Column leftKeyColumn, Column rightKeyColumn) 
     {
@@ -20,13 +20,26 @@ public sealed class JoinLens : SymmetricTableLens
         _rightKeyColumn = rightKeyColumn;
     }
 
+    public Func<Table, Option<(Table, Table)>, Result<(Table, Table)>> PutLeft => 
+        (updatedSource, originalTarget) 
+            => originalTarget.Match(
+                target => Result.Success(target),
+                () => CreateLeft(updatedSource)
+            );
+    public Func<(Table, Table), Option<Table>, Result<Table>> PutRight => 
+        (updatedSource, originalTarget) 
+            => originalTarget.Match(
+                target => Result.Success(Table.Cons(_targetTableName, updatedSource.Item1.Columns.Concat(updatedSource.Item2.Columns))),
+                () => CreateRight(updatedSource)
+            );
+                
 
-    public override Func<Table, Option<Table>, Result<Table>> PutLeft => throw new NotImplementedException();
+    public Func<(Table, Table), Result<Table>> CreateRight => 
+        source => Table.Cons(_targetTableName, source.Item1.Columns.Concat(source.Item2.Columns));
 
-    public override Func<Table, Option<Table>, Result<Table>> PutRight => throw new NotImplementedException();
-
-    public override Func<Table, Result<Table>> CreateRight => 
-        table => _rightTableLens.CreateRight(table).Map(rightTable => Table.Cons(TargetTableName, rightTable.Columns));
-
-    public override Func<Table, Result<Table>> CreateLeft => throw new NotImplementedException();
+    public Func<Table, Result<(Table, Table)>> CreateLeft => 
+        source => Result.Success(
+            (Table.Cons("table1", source.Columns.TakeWhile(col => !col.Equals(_leftKeyColumn))), 
+             Table.Cons("table2", source.Columns.SkipWhile(col => !col.Equals(_leftKeyColumn))))
+            );
 }
