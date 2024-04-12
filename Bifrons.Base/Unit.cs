0c8fd2c3715b4e25e5eb2c42556caceb9a2d82c7
JurajDoncevic <juraj.doncevic@fer.hr>
2024-04-12 14:15:08 +0200

    Add Covalesce method to UnitExt class

--- Bifrons.Base/Unit.cs
+++ Bifrons.Base/Unit.cs
@@ -23,4 +23,15 @@ public static class UnitExt
     /// <param name="_"></param>
     /// <returns>A Unit value</returns>
     public static Unit Ignore<T>(this T target) => Unit();
+
+    public static TData Covalesce<TData>(this Unit target)
+        => default(TData) ?? typeof(TData) switch
+    {
+        var type when type == typeof(string) => (TData)(object)string.Empty,
+        var type when type == typeof(int) => (TData)(object)0,
+        var type when type == typeof(double) => (TData)(object)0.0,
+        var type when type == typeof(bool) => (TData)(object)false,
+        var type when type == typeof(DateTime) => (TData)(object)DateTime.MinValue,
+        _ => throw new NotSupportedException($"Unsupported type: {typeof(TData)}")
+    };
 }
