fae625df7e5507f9fd529b06648f422b6bbd1323
JurajDoncevic <juraj.doncevic@outlook.com>
2024-06-07 21:22:46 +0200

    removing mssql as a viable experiment option

--- Bifrons.Cannonizers.Relational.Mssql.Tests/DatabaseCollection.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Bifrons.Cannonizers.Relational.Mssql.Tests;
-
-[CollectionDefinition("Database collection")]
-public class DatabaseCollection : ICollectionFixture<DatabaseFixture>
-{
-    // This class has no code, and is never created. Its purpose is simply
-    // to be the place to apply [CollectionDefinition] and all the
-    // ICollectionFixture<> interfaces.
-}
--- Bifrons.Cannonizers.Relational.Mssql/Usings.cs
+++
@@ -1,2 +0,0 @@
-﻿global using static Bifrons.Base.UnitExt;
-global using Bifrons.Base;
