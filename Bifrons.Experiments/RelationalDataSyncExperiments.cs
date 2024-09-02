6e390fcb0983ad1035d79a08b6f91a5ff05d4831
JurajDoncevic <juraj.doncevic@fer.hr>
2024-09-02 10:39:27 +0200

    experiment updates

--- Bifrons.Experiments/RelationalDataSyncExperiments.cs
+++ Bifrons.Experiments/RelationalDataSyncExperiments.cs
@@ -322,16 +322,23 @@ public sealed class RelationalDataSyncExperiments : IClassFixture<DatabaseFixtur
 
         // ACT
         var res_tblData_rightStudents = tblDataLens_students.PutRight(tblDataLeft_students, tblDataRight_students);
+        
+        var res_financialDataSync = res_tblData_rightStudents
+            .Bind(students => financialCannonizer.CommandManager.SyncIntoDatabase(students.Table, new List<Column> { tblRight_students["StudentID"].Value }, students.RowData));
 
-        var res_tblData_leftStudents = tblDataLens_students.PutLeft(tblDataRight_students, tblDataLeft_students);
+        var rightTblData_students = financialCannonizer.QueryManager.GetAllFrom(tblRight_students)
+            .Match(
+                data => data,
+                error => throw new Exception(error)
+            );
+
 
         // ASSERT
         Assert.True(res_tblData_rightStudents);
-        Assert.Equal(tblDataLeft_students.RowData.Count, tblDataRight_students.RowData.Count);
+        Assert.True(res_financialDataSync);
+        Assert.Equal(tblDataLeft_students.RowData.Count, rightTblData_students.RowData.Count);
 
-        Assert.True(res_tblData_leftStudents);
-        Assert.Equal(tblDataLeft_students.RowData.Count, tblDataRight_students.RowData.Count);
-
+    
         // CLEANUP
         // delete data from left/academic database
         var res_academicDeletion = newAcademicStudentData.Fold(
@@ -351,6 +358,180 @@ public sealed class RelationalDataSyncExperiments : IClassFixture<DatabaseFixtur
             error => throw new Exception(error)
         );
 
+    }
+
+    [Fact]
+    public void Synchronize_StudentTableData_WhenRightDataIsAdded()
+    {
+        #region ARRANGE
+        // ARRANGE
+        // setup cannonizers
+        // left
+        var academicCannonizer = _fixture.GetService<ICannonizer>("AcademicCannonizer");
+        // right
+        var financialCannonizer = _fixture.GetService<ICannonizer>("FinancialCannonizer");
+
+        // create student table structure lens
+        var colLens_studentId = Columns.IdentityLens.Cons("StudentID");
+        var colLens_firstName = Columns.IdentityLens.Cons("FirstName");
+        var colLens_lastName = Columns.IdentityLens.Cons("LastName");
+        var colLens_email = Columns.IdentityLens.Cons("Email");
+        var colLens_phoneNumber = Columns.IdentityLens.Cons("PhoneNumber");
+        var colLens_major = Columns.DeleteLens.Cons("Major", Relational.Model.DataTypes.STRING);
+        var colLens_enrollmentDate = Columns.DeleteLens.Cons("EnrollmentDate", Relational.Model.DataTypes.DATETIME);
+        var colLens_billingAddress = Columns.InsertLens.Cons("BillingAddress", Relational.Model.DataTypes.STRING);
+        var tblLens_students = Tables.IdentityLens.Cons(
+            "Students",
+            new List<Relational.Columns.SymmetricColumnLens>
+            {
+                colLens_studentId,
+                colLens_firstName,
+                colLens_lastName,
+                colLens_email,
+                colLens_phoneNumber,
+                colLens_major,
+                colLens_enrollmentDate,
+                colLens_billingAddress
+            });
+
+        // create student table data lens
+        var colDataLens_studentId = DataColumns.StringIdentityLens.Cons(
+            colLens_studentId,
+            Strings.IdentityLens.Cons()
+            );
+        var colDataLens_firstName = DataColumns.StringIdentityLens.Cons(
+            colLens_firstName,
+            Strings.IdentityLens.Cons()
+            );
+        var colDataLens_lastName = DataColumns.StringIdentityLens.Cons(
+            colLens_lastName,
+            Strings.IdentityLens.Cons()
+            );
+        var colDataLens_email = DataColumns.StringIdentityLens.Cons(
+            colLens_email,
+            Strings.IdentityLens.Cons()
+            );
+        var colDataLens_phoneNumber = DataColumns.StringIdentityLens.Cons(
+            colLens_phoneNumber,
+            Strings.IdentityLens.Cons()
+            );
+        var colDataLens_major = DataColumns.StringDeleteLens.Cons(
+            colLens_major,
+            "N/A"
+            );
+        var colDataLens_enrollmentDate = DataColumns.DateTimeDeleteLens.Cons(
+            colLens_enrollmentDate,
+            DateTime.MinValue
+            );
+        var colDataLens_billingAddress = DataColumns.StringInsertLens.Cons(
+            colLens_billingAddress,
+            "N/A"
+            );
+        var tblDataLens_students = DataTables.IdentityLens.Cons(
+            tblLens_students,
+            new List<RelationalData.Columns.ISymmetricColumnDataLens>
+            {
+                colDataLens_studentId,
+                colDataLens_firstName,
+                colDataLens_lastName,
+                colDataLens_email,
+                colDataLens_phoneNumber,
+                colDataLens_major,
+                colDataLens_enrollmentDate,
+                colDataLens_billingAddress
+            },
+            new List<Column>() { Column.Cons("StudentID", Relational.Model.DataTypes.STRING) }
+            ).Match(
+                lens => lens,
+                error => throw new Exception(error)
+            );
+        
+        // load left student table model
+        var tblLeft_students = academicCannonizer.MetadataManager.GetTable("Students")
+            .Match(
+                table => table,
+                error => throw new Exception(error)
+            );
+        // load right student table model
+        var tblRight_students = financialCannonizer.MetadataManager.GetTable("Students")
+            .Match(
+                table => table,
+                error => throw new Exception(error)
+            );
+        
+        // insert new data to right/financial database
+        var newFinancialStudentData = Enumerable.AsEnumerable(
+        [
+            RowData.Cons(
+                    new List<ColumnData>
+                    {
+                        ColumnData.Cons(tblRight_students["StudentID"].Value, "QQQQQQQQQQ").Match(_ => _, error => throw new Exception(error)),
+                        ColumnData.Cons(tblRight_students["FirstName"].Value, "Tiff").Match(_ => _, error => throw new Exception(error)),
+                        ColumnData.Cons(tblRight_students["LastName"].Value, "Shaw").Match(_ => _, error => throw new Exception(error)),
+                        ColumnData.Cons(tblRight_students["Email"].Value, "tiff.shaw@example.com").Match(_ => _, error => throw new Exception(error)),
+                        ColumnData.Cons(tblRight_students["PhoneNumber"].Value, "123-456-7777").Match(_ => _, error => throw new Exception(error)),
+                        ColumnData.Cons(tblRight_students["BillingAddress"].Value, "123 Village St, City, State, ZIP").Match(_ => _, error => throw new Exception(error))
+                    }),
+            RowData.Cons(
+                    new List<ColumnData>
+                    {
+                        ColumnData.Cons(tblRight_students["StudentID"].Value, "PPPPPPPPPP").Match(_ => _, error => throw new Exception(error)),
+                        ColumnData.Cons(tblRight_students["FirstName"].Value, "Tim").Match(_ => _, error => throw new Exception(error)),
+                        ColumnData.Cons(tblRight_students["LastName"].Value, "Needell").Match(_ => _, error => throw new Exception(error)),
+                        ColumnData.Cons(tblRight_students["Email"].Value, "tim.needell@example.com").Match(_ => _, error => throw new Exception(error)),
+                        ColumnData.Cons(tblRight_students["PhoneNumber"].Value, "765-432-1111").Match(_ => _, error => throw new Exception(error)),
+                        ColumnData.Cons(tblRight_students["BillingAddress"].Value, "456 Village St, City, State, ZIP").Match(_ => _, error => throw new Exception(error))
+                    })
+        ]);
+
+        var res_financialInsertion = newFinancialStudentData.Fold(
+            Enumerable.Empty<Result<Unit>>(),
+            (rowData, acc) => acc.Append(financialCannonizer.CommandManager.Insert(tblRight_students, rowData))
+        ).Unfold();
+
+        // load left student data
+        var tblDataLeft_students = academicCannonizer.QueryManager.GetAllFrom(tblLeft_students)
+            .Match(
+                data => data,
+                error => throw new Exception(error)
+            );
+        // load right student data
+        var tblDataRight_students = financialCannonizer.QueryManager.GetAllFrom(tblRight_students)
+            .Match(
+                data => data,
+                error => throw new Exception(error)
+            );
+        #endregion
+
+        // ACT
+        var res_tblData_leftStudents = tblDataLens_students.PutLeft(tblDataRight_students, tblDataLeft_students);
+
+        var res_academicDataSync = res_tblData_leftStudents
+            .Bind(students => academicCannonizer.CommandManager.SyncIntoDatabase(students.Table, new List<Column> { tblLeft_students["StudentID"].Value }, students.RowData));
+        
+        var leftTblData_students = academicCannonizer.QueryManager.GetAllFrom(tblLeft_students)
+            .Match(
+                data => data,
+                error => throw new Exception(error)
+            );
+
+        // ASSERT
+        Assert.True(res_tblData_leftStudents);
+        Assert.True(res_academicDataSync);
+        Assert.Equal(leftTblData_students.RowData.Count, tblDataRight_students.RowData.Count);
+
+        // CLEANUP
+        // delete data from left/academic database
+        var res_academicDeletion = newFinancialStudentData.Fold(
+            Enumerable.Empty<Result<Unit>>(),
+            (rowData, acc) => acc.Append(academicCannonizer.CommandManager.Delete(tblLeft_students, rd => rd["StudentID"] == rowData["StudentID"]))
+        ).Unfold();
+
+        // delete data from right/financial database
+        var res_financialDeletion = newFinancialStudentData.Fold( // doesn't matter if we delete from financial or academic database, since we only use IDs
+            Enumerable.Empty<Result<Unit>>(),
+            (rowData, acc) => acc.Append(financialCannonizer.CommandManager.Delete(tblRight_students, rd => rd["StudentID"] == rowData["StudentID"]))
+        ).Unfold();
     }
 
     [Fact]
