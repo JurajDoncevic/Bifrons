0b8deaa4c6ddcbe039c91cfa586ba80309bf5d47
JurajDoncevic <juraj.doncevic@outlook.com>
2025-01-02 17:13:30 +0100

    added structural lens example

--- Bifrons.Experiments/PaperExamples.cs
+++ Bifrons.Experiments/PaperExamples.cs
@@ -3,6 +3,8 @@ using Integers = Bifrons.Lenses.Integers;
 using Strings = Bifrons.Lenses.Strings;
 using CrossType = Bifrons.Lenses.CrossType;
 using DateTimes = Bifrons.Lenses.DateTimes;
+using Columns = Bifrons.Lenses.Relational.Columns;
+using Tables = Bifrons.Lenses.Relational.Tables;
 using Bifrons.Lenses;
 
 
@@ -118,5 +120,45 @@ public sealed class PaperExamples
         Assert.True(res_returnOriginalDateString);
         Assert.True(res_returnOriginalDateString.Data.IsLeft);
         Assert.Equal(dateTimeString, res_returnOriginalDateString.Data);
+    }
+
+    [Fact]
+    public void Paper_RelationalLensesExample()
+    {
+        //------------------ People table ------------------
+        var col_id = Columns.IdentityLens.Cons("Id");
+        var col_firstName = Columns.IdentityLens.Cons("FirstName");
+        var col_lastName = Columns.IdentityLens.Cons("LastName");
+        var col_dateOfBirth = Columns.RenameLens.Cons("DateOfBirth", "DoB");
+        var col_address = Columns.DeleteLens.Cons("Address");
+        var col_email = Columns.InsertLens.Cons("Email");
+        var col_phone = Columns.DeleteLens.Cons("Phone");
+        var tbl_people = Tables.IdentityLens.Cons("People", col_id, col_firstName, col_lastName, col_dateOfBirth, col_address, col_email, col_phone);
+
+        //------------------ Roles table ------------------
+        var col_roleId = Columns.IdentityLens.Cons("Id");
+        var col_roleName = Columns.IdentityLens.Cons("RoleName");
+        var tbl_roles = Tables.IdentityLens.Cons("Roles", col_roleId, col_roleName);
+
+        //------------------ PeopleRoles table ------------------
+        var col_peopleRolesId = Columns.IdentityLens.Cons("Id");
+        var col_peopleId = Columns.IdentityLens.Cons("PeopleId");
+        var col_roleId2 = Columns.IdentityLens.Cons("RoleId");
+        var tbl_peopleRoles = Tables.IdentityLens.Cons("PeopleRoles", col_peopleRolesId, col_peopleId, col_roleId2);
+
+        //------------------ Departments table ------------------
+        var col_departmentId = Columns.IdentityLens.Cons("Id");
+        var col_departmentName = Columns.IdentityLens.Cons("DepartmentName");
+        var tbl_departments = Tables.IdentityLens.Cons("Departments", col_departmentId, col_departmentName);
+
+        //------------------ Users table ------------------
+        var tbl_users = Tables.DeleteLens.Cons("Users");
+
+
+        Assert.NotNull(tbl_people);
+        Assert.NotNull(tbl_roles);
+        Assert.NotNull(tbl_peopleRoles);
+        Assert.NotNull(tbl_departments);
+        Assert.NotNull(tbl_users);
     }
 }
